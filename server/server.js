50ef976174986751a766775fb684925dfbc13c54
htlin2 <htlin2@gmail.com>
2018-11-25 15:33:55 -0800

    updated proxy server

--- server/server.js
+++ server/server.js
@@ -3,34 +3,34 @@ const httpProxy = require('http-proxy');
 const port = 8080;
 
 const apiProxy = httpProxy.createProxyServer();
-const serverOne = 'http://ec2-18-222-111-239.us-east-2.compute.amazonaws.com/';  //serves mortgage calculator
-const serverTwo = 'http://ec2-34-232-109-92.compute-1.amazonaws.com/';  //serves home description
-const serverThree = 'http://ec2-13-56-210-238.us-west-1.compute.amazonaws.com/'; //serves nearby homes
-const serverFour = 'http://ec2-52-53-188-116.us-west-1.compute.amazonaws.com/'; //serves image carousel
+const serverOne = 'http://htlin.io/';  //serves mortgage calculator
+// const serverTwo = 'http://localhost:3002';  //serves home description
+// const serverThree = 'http://localhost:3003'; //serves nearby homes
+// const serverFour = 'http://localhost:3004'; //serves image carousel
 
 const app = express();
 
-app.use(express.static('./public'));
+app.use('/homes/:id', express.static('./public'));
 
-app.all("/api/homes/1/prices", function(req, res) {
+app.all("/api/homes/:id/prices", function (req, res) {
   console.log('redirecting to Server1');
-  apiProxy.web(req, res, {target: serverOne});
+  apiProxy.web(req, res, { target: serverOne });
 });
 
-app.all("/api/homes/:index/detail-information", function(req, res) {
-  console.log('redirecting to Server2');
-  apiProxy.web(req, res, {target: serverTwo});
-});
+// app.all('/api/homes/:index/detail-information', function(req, res) {
+//   console.log('redirecting to Server2');
+//   apiProxy.web(req, res, {target: serverTwo});
+// });
 
-app.all("/nearbyhomes", function(req, res) {
-  console.log('redirecting to Server3');
-  apiProxy.web(req, res, {target: serverThree});
-});
+// app.all('/nearbyhomes', function(req, res) {
+//   console.log('redirecting to Server3');
+//   apiProxy.web(req, res, {target: serverThree});
+// });
 
-app.all("/images/:houseID", function(req, res) {
-  console.log('redirecting to Server4');
-  apiProxy.web(req, res, {target: serverFour});
-});
+// app.all('/images/:houseID', function(req, res) {
+//   console.log('redirecting to Server4');
+//   apiProxy.web(req, res, {target: serverFour});
+// });
 
 app.listen(port, () => {
   console.log(`listening at ${port}`);
